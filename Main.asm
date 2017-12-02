2638d6166d65259a198d2892cb351a3d1eb2cc52
Josh Klassen <joshuaklassen17@gmail.com>
2017-12-02 17:24:36 -0600

    Print 'hello' to the screen

--- Main.asm
+++ Main.asm
@@ -150,27 +150,156 @@ SECTION "Program Start", ROM0[$150]
 
 Main::
 
+.init
+
 	DI					;disable interrupts
 
 	LD sp, $FFFE		;reset stack pointer
 
-	LD a, mSetLowBit	;force vblank interrupt
-	LD [rIE], a
+	call WAIT_VBLANK
+	call LCD_OFF
+
+	call CLEAR_SCREEN
+
+	LD a, mBgPalette	;load our palette
+	LD [rBGP], a
+
+	ld hl, Hello_Tiles
+	ld bc, 9*16
+	call LOAD_TILE
+
+	call LOAD_MAP
+
+	call LCD_ON
+
 	EI
 
 .main_loop
 	
-	INC A
+	halt
 	nop
 
 	jp .main_loop
 
 
+WAIT_VBLANK::
+	ld a, [rLY]			;load y cord of scanline
+	cp 145				;line 145 = we are in vblank
+	jr nz, WAIT_VBLANK
+	ret
+
+UPDATE_GAME:
+	ret
+
+CLEAR_SCREEN:
+	ld hl, lSCRN0
+	ld bc, 32*32
+.clear_screen_loop
+	ld a,$0
+	ldi [hl], a
+	dec bc
+	ld a, b
+	or c
+	jr nz, .clear_screen_loop
+
+	ret
+
+;----------------------------
+;LOAD_TILE
+;
+; loads a tile into VRAM
+; parameters: 
+;		hl: start of tile(s)
+; 		bc: number of bytes
+;
+;----------------------------
+
+LOAD_TILE:
+	push af
+	push de
+
+	ld de, lVRAM
+
+.load_tile_loop
+	ld a,[hl]
+	inc hl
+	ld [de], a
+	inc de
+	dec bc
+	ld a,b
+	or c
+	jr nz, .load_tile_loop
+
+	pop de
+	pop af
+
+	ret
+
+LOAD_MAP:
+	ld hl, Hello_Map
+	ld de, lSCRN0
+	ld c, 5
+.load_map_loop
+	ld a,[hl]
+	inc hl
+	ld [de], a
+	inc de
+	dec c
+	jr nz, .load_map_loop
+	ret
+
+
+LCD_OFF:
+	
+	ld a, [rLCDC]
+	res 7, a
+	ld [rLCDC], a
+	ret
+
+LCD_ON:
+
+	ld a, [rLCDC]
+	set 7, a
+	ld [rLCDC], a
+	ret
+
+My_Tile:
+DB $7C,$7C,$00,$C6,$C6,$00,$00,$FE,$C6,$C6,$00,$C6,$C6,$00,$00,$00
+
+Hello_Tiles:
+DB $00,$00,$00,$00,$00,$00,$00,$00
+DB $00,$00,$00,$00,$00,$00,$00,$00
+DB $C6,$C6,$C6,$C6,$C6,$C6,$FE,$FE
+DB $FE,$FE,$C6,$C6,$C6,$C6,$C6,$C6
+DB $FE,$FE,$FE,$FE,$80,$80,$F8,$F8
+DB $F8,$F8,$80,$80,$FE,$FE,$FE,$FE
+DB $C0,$C0,$C0,$C0,$C0,$C0,$C0,$C0
+DB $C0,$C0,$C0,$C0,$FE,$FE,$FE,$FE
+DB $7C,$7C,$FE,$FE,$C6,$C6,$C6,$C6
+DB $C6,$C6,$C6,$C6,$FE,$FE,$7C,$7C
+DB $C6,$C6,$C6,$C6,$C6,$C6,$C6,$C6
+DB $D6,$D6,$D6,$D6,$FE,$FE,$6C,$6C
+DB $FC,$FC,$FE,$FE,$C6,$C6,$FC,$FC
+DB $FC,$FC,$C6,$C6,$C6,$C6,$C6,$C6
+DB $FC,$FC,$FE,$FE,$C6,$C6,$C6,$C6
+DB $C6,$C6,$C6,$C6,$FE,$FE,$FC,$FC
+DB $6C,$6C,$6C,$6C,$6C,$6C,$6C,$6C
+DB $6C,$6C,$6C,$6C,$00,$00,$6C,$6C
+
+Hello_Map:
+DB $01,$02,$03,$03,$04
 
 ;---------------------------
 ;interrupt service routines
 ;---------------------------
 VBlankInterrupt:
+
+	PUSH af				;save regs
+	PUSH bc
+
+	POP bc				;load regs
+	POP af
+
 	reti
 LCDInterrupt:
 	reti
--- constants.asm
+++ constants.asm
@@ -1,10 +1,22 @@
+; locations
+lVRAM			EQU		$8000
+lSCRN0	EQU		$9800
+
 ; Hardware registers
 
 rLCDC 	EQU 	$ff40
+rSTAT	EQU		$ff41
 rSCY 	EQU		$ff42
 rSCX	EQU		$ff43
+rLY		EQU		$ff44
+rLYC	EQU		$ff45
+rBGP	EQU		$ff47
+
+rWY		EQU		$ff4a
+
 rIE		EQU		$ffff
 
 ; Masks
 
 mSetLowBit	EQU		%00000001
+mBgPalette	EQU		%11100100
